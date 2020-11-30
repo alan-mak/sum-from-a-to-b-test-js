ad8302beea55765fee932b7ba73999f5c32c88eb
Alan <alanmak95@gmail.com>
2020-11-30 00:00:42 +0000

    Solved problem using recursion

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  // Base Case
+  if(toN === fromN) {
+    return fromN;
+  }
+  // Recursive Case
+  return toN + sum(fromN, toN - 1)
 }
 
 module.exports = sum;
